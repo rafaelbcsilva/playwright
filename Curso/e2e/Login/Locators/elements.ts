ef81dbefcbb42e7cd765c46f535300ca0ca4df69
Rafael Bezerra <rafael.bezerra@external.stellantis.com>
2025-05-14 01:53:48 -0300

    Aplicando meu tipo de Page Object com Playwright

--- Curso/e2e/Login/Locators/elements.ts
+++
@@ -1,5 +0,0 @@
-export const ELEMENTS = {
-
-    inputEmail: 'hsjsjejes'
-
-}
--- Curso/e2e/specs/visitarPaginaInicial.spec.ts
+++ Curso/e2e/specs/visitarPaginaInicial.spec.ts
@@ -2,8 +2,13 @@ import test, { expect } from "@playwright/test";
 
 test.describe('Pagina Inicial', () => {
     test('Deve visitar a pagina inicial', async ({ page }) => {
-        await page.goto('http://localhost:4200/');
+        await page.goto('/');
         await expect(page).toHaveTitle('Jornada Milhas');
+
+        // const tituloPassagens = page.getByRole('heading', { name: 'Passagens' });
+        //await expect(tituloPassagens).toBeVisible();
+        const tituloPassagens = page.getByTestId('titulo-passagens');
+        await expect(tituloPassagens).toBeVisible();
     });
 
 });
